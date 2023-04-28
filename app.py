204f4194706342955f2e615945d3616725551e1a
lagordarusa <lagordarusa@Gmail.com.com>
2023-04-28 09:46:35 -0400

    28/04/2023_02

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Cancion:
     def __init__(self,titulo,categoria,idioma):
@@ -6,15 +6,15 @@ class Cancion:
         self.categoria= categoria
         self.idioma=idioma
 
+cancion1=Cancion('La guitarra','pop','castellano')
+cancion2=Cancion('para no verte mas','pop','castellano')
+cancion3=Cancion('balada para un gordo','balada','castellano')
+lista=[cancion1,cancion2,cancion3]    
+        
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-    cancion1=Cancion('La guitarra','pop','castellano')
-    cancion2=Cancion('para no verte mas','pop','castellano')
-    cancion3=Cancion('balada para un gordo','balada','castellano')
-    lista=[cancion1,cancion2,cancion3]    
     return render_template('listar.html', titulo='Canciones',musicas=lista)
 
 @app.route('/nuevoregistro')
@@ -22,6 +22,18 @@ def nuevoRegistro():
     return render_template('nuevoRegistro.html', titulo='Nueva Canción')
     #return 'estoy en nuevo registro' 
 
+
+@app.route('/crear', methods=['POST',])
+def crear():
+    titulo = request.form['titulo']
+    categoria = request.form['categoria']
+    idioma = request.form['idioma']
+    cancion = Cancion(titulo, categoria, idioma)
+    lista.append(cancion)
+    return render_template('listar.html', titulo='Canciones', musicas=lista)
+    
+    
+    
 
   
 app.run(host="0.0.0.0", port=5000)
